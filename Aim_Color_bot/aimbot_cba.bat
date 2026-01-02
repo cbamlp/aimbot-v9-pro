af8ae74067018630cb69bbf3df252cc06716f8ab
cbamlp <cbamlp53@gmail.com>
2026-01-02 13:58:29 -0300

    Add files via upload

---
+++ Aim_Color_bot/aimbot_cba.bat
@@ -0,0 +1,28 @@
+@echo off
+title AIMBOT MODULE v9.3 - PRIVATE
+color 0A
+
+echo Initializing Aimbot Core...
+timeout /t 1 >nul
+echo Loading color matrix...
+timeout /t 1 >nul
+echo Humanizer enabled
+timeout /t 1 >nul
+echo Anti-detect ACTIVE
+timeout /t 1 >nul
+echo.
+echo [!] Enemy detected - HEAD
+timeout /t 1 >nul
+echo [✓] Aim locked
+timeout /t 1 >nul
+echo [✓] Target eliminated
+timeout /t 1 >nul
+echo.
+
+set /p license=License key: 
+
+:: Ventana de troleo
+start cmd /k "title LOL & color 0C & echo TE FUISTE TROLEADO & echo. & pause"
+
+:: Abre el video de YouTube
+start https://www.youtube.com/watch?v=Rmb_7fVESck
